6d13d7fec7fdbe40287ad7905dd069029171f0a0
OliviaZhu26 <30611503+OliviaZhu26@users.noreply.github.com>
2018-06-28 13:15:51 +0800

    Update Sam_to_bam.pl

--- BAsE-Seq_scripts/Sam_to_bam.pl
+++ BAsE-Seq_scripts/Sam_to_bam.pl
@@ -1,7 +1,11 @@
 use warnings;
 use strict;
 
-### CONVERT INDIVIDUAL GENOME ALIGNMENT FILES FROM SAM TO BAM
+##Author: Lewis HONG and ZHU O. Yuan
+##Script name: Sam_to_bam.pl
+##CONVERT INDIVIDUAL GENOME ALIGNMENT FILES FROM SAM TO BAM
+##Run as: perl $SCRIPTPATH/Sam_to_bam.pl $DATAPATH
+
 my$DIRECTORY=$ARGV[0];
 
 system "mkdir $DIRECTORY/IndividualGenomes/bam";
@@ -9,6 +13,7 @@ opendir(SAM, "$DIRECTORY/IndividualGenomes/sam/");
 my @files = readdir(SAM);
 foreach my $file (@files) {
 	if ($file =~ /(\w+)\.sam/) {
+		#converts each individual sam to sorted bam in a new bam folder
 		my $cmd = "samtools view -buS $DIRECTORY/IndividualGenomes/sam/$file | samtools sort -  $DIRECTORY/IndividualGenomes/bam/$1_sorted";
 		print "Processing $file\n";
 		system $cmd;
