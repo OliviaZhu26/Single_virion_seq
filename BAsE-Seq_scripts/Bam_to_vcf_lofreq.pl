f0abfef9f5cabac8879dad1e90d9f8859e49a3d1
OliviaZhu26 <30611503+OliviaZhu26@users.noreply.github.com>
2018-06-28 13:25:53 +0800

    Update Bam_to_vcf_lofreq.pl

--- BAsE-Seq_scripts/Bam_to_vcf_lofreq.pl
+++ BAsE-Seq_scripts/Bam_to_vcf_lofreq.pl
@@ -1,18 +1,21 @@
 use warnings;
 use strict;
 
-my$REF=$ARGV[0];
-my$DIRECTORY=$ARGV[1];
+##Author: Lewis HONG and ZHU O. Yuan
+##Script name: Bam_to_vcf_lofreq.pl
+##OBTAIN VCF FILES FROM BAM FILES
+##Run as: perl $SCRIPTPATH/Bam_to_vcf_lofreq.pl $REFPATH $DATAPATH
 
-### OBTAIN VCF FILES FROM BAM FILES
+my$REF=$ARGV[0]; #reference fasta for lofreq to call SNPs off of
+my$DIRECTORY=$ARGV[1]; #path to sample files
 
-#system "mkdir $DIRECTORY/IndividualGenomes/lofreq_vcf/";
-opendir(BAM, "sorted_bam_rmdup/");
+system "mkdir $DIRECTORY/IndividualGenomes/lofreq_vcf/"; #creates new folder for vcf files
+opendir(BAM, "sorted_bam_rmdup/"); #reads in sorted rmdup bams to lofreq caller
 my @files = readdir(BAM);
 foreach my $file (@files) {
 	if ($file =~ /(\w+)_rmdup\.bam$/) {
 		my $cmd = "lofreq call -D -f $REF sorted_bam_rmdup/$file -o lofreq_vcf/$1.vcf";
-		print "Processing $file\n";
+		print "SNP calling on $file with Lofreq\n";
 		my @cmds = ("bash", "-c", $cmd);
 		system (@cmds);
 	}
