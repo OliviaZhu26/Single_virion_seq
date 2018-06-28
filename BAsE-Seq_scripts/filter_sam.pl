c616adc713050f27dfba8805ab7f3be28ae25def
OliviaZhu26 <30611503+OliviaZhu26@users.noreply.github.com>
2018-06-28 12:27:51 +0800

    Update filter_sam.pl

--- BAsE-Seq_scripts/filter_sam.pl
+++ BAsE-Seq_scripts/filter_sam.pl
@@ -1,22 +1,19 @@
 use warnings;
 use strict;
 
-### CREATE A SAM FILE FOR CONCORDANT ALIGNMENTS ###
+## Authors: Lewis HONG and ZHU O. Yuan
+## Script filter_sam.pl
+## CREATE A SAM FILE FOR CONCORDANT ALIGNMENTS
+## Run as: perl $SCRIPTPATH/filter_sam.pl $DATAPATH/$PREFX.sam > $DATAPATH/$PREFX.filtered.sam
 
-my $concordant;
-my $discordant;
-my $mapped;
-my $count = 0;
-
-open (SAM, "$ARGV[0]") or die; #input *_pf_bwa.sam
+open (SAM, "$ARGV[0]") or die;
 while (my $line = <SAM>) {
 	if ($line =~ /(^@.*\n)/) {
-		print "$line";
+		print "$line"; #print all sam header lines
 	}
-	$count++;
 	if ($line =~ /^.*?\t(\d+)\t/) {
 		if ($1 == 99 || $1 == 147) { #if barcode is in read 2, read 1 should be in forward and read 2 should be in reverse
-			print "$line";
+			print "$line"; #print only concordant alignments
 		}
 	}
 }
