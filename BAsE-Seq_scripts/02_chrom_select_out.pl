9bd53ebb6575d40e680ca9716236bc2fbba70c3f
OliviaZhu26 <30611503+OliviaZhu26@users.noreply.github.com>
2018-06-28 14:30:11 +0800

    Update 02_chrom_select_out.pl

--- BAsE-Seq_scripts/02_chrom_select_out.pl
+++ BAsE-Seq_scripts/02_chrom_select_out.pl
@@ -1,17 +1,23 @@
 #!/user/bin/perl
 use warnings;
 use strict;
-#open OUT, "> $ARGV[1]" or die("Couldn't open $ARGV[1]\n");
+
+##Author: ZHU O. Yuan (Genome Institute of Singapore)
+##Script name: 02_chrom_select_out.pl
+##selects best match genotype reference from a list 
+##Run as: perl $scriptpath/02_chrom_select_out.pl 02_mismatches.txt > 03_best_match_chroms.txt
 
 my%simi=();my$ref="";
 open(TXT, $ARGV[0]);
 while(<TXT>){
     my$line=$_; chomp $line;
     my@reads=split('\t',$line);
+    #stores all mismatch values for all genotypes
     if(defined $reads[1]){$simi{$ref}=$reads[0];}
     else{$ref=$reads[0];}
 }close(TXT);
 
+#prints out the lowest mismatch genotype
 my$lowest=1;
 foreach my $key (keys %simi){
     if($simi{$key}<$lowest){$lowest=$simi{$key};}
