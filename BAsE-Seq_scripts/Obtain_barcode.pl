97a452cbbc5531a11996dd6d786325ed6d1cebdb
OliviaZhu26 <30611503+OliviaZhu26@users.noreply.github.com>
2018-06-28 12:11:54 +0800

    Update Obtain_barcode.pl

--- BAsE-Seq_scripts/Obtain_barcode.pl
+++ BAsE-Seq_scripts/Obtain_barcode.pl
@@ -1,46 +1,53 @@
 use warnings;
 use strict;
-### OBTAIN BARCODE-ASSOCIATED READS
-### BARCODE-CONTAINING READS ARE ANCHORED BY A 20 BP BARCODE AND UNI-A SEQUENCE UP TO THE SBFI SITE
-### perl ../lch_base_scripts/Obtain_barcode.pl R1_001.fastq R2_001.fastq New_01_barcoded_R1.fastq New_01_barcoded.R2.fastq
+## Authors: Lewis HONG and ZHU O. Yuan
+## Script Obtain_barcode.pl
+## OBTAIN BARCODE-ASSOCIATED READS ONLY
+## BARCODE-CONTAINING READS ARE ANCHORED BY A 20 BP BARCODE AND UNI-A SEQUENCE UP TO THE SBFI SITE
+## Run as: perl Obtain_barcode.pl R1_001.fastq R2_001.fastq New_01_barcoded_R1.fastq New_01_barcoded.R2.fastq 20 CGAC
 
+#assign variables
 my$LEN=$ARGV[4];
 my$CODE=$ARGV[5];
-my$tracker=0;my%pas=();
-my$line_number=0;
-my$pass_filter=0;
-my($line1,$line2,$line3,$line4);
+my$tracker=0; #variable tracking line number in entire fastq file
+my%pas=(); #hash to store line information on passed reads
+my$line_number=0; #variable tracking line number in each set of 4 lines
+my$pass_filter=0; #tracks whether read being assessed passes required format
+my($line1,$line2,$line3,$line4); #temporary storage for the 4 lines of fastq information for each read being assessed
 
-open(OUT1,">$ARGV[3]"); #output file1
-open(FASTA, $ARGV[1]);
+open(OUT1,">$ARGV[3]"); #output barcoded_R2.fq
+open(FASTA, $ARGV[1]); #input R2.fq
 while(my$line=<FASTA>){
-    $line_number++;$tracker++;#print"$line_number\t$tracker\n";
-    if($line_number==1){$line1=$line;}
+    $line_number++;
+    $tracker++;
+    if($line_number==1){$line1=$line;} #stores read header info
     if($line_number==2){
-	#print substr($line,20,4)."\n";#print "$line\n";
 	if($line =~ /^([ATCGN]{$LEN})$CODE/){
-	    #print "OK\n";
-	    $line2=$line;
-	    $pass_filter=1;
-	    #print"$pass_filter\n";
+	    #if read begins with a string of nucleotides 20bp long followed by the known barcode
+	    $line2=$line; #store read sequence
+	    $pass_filter=1; #note that read contains required barcode
 	}
     }
-    if($line_number==3){$line3=$line;}
+    if($line_number==3){$line3=$line;} #stores "+"
     if($line_number==4){
 	$line4=$line;
 	if($pass_filter==1){
+	#if read contains required barcode, print read to output file
 	    print OUT1 "$line1$line2$line3$line4";
+	    #store line numbers of passed reads
 	    $pas{($tracker)}=1;$pas{($tracker-1)}=1;$pas{($tracker-2)}=1;$pas{($tracker-3)}=1;
 	}
+	#reset variables
 	$line_number = 0;
 	$pass_filter = 0;
     }	
 }close(OUT1);
 
 $tracker=0;
-open(OUT2,">$ARGV[2]"); #output file2
-open(FASTB, $ARGV[0]);
+open(OUT2,">$ARGV[2]"); #output barcoded_R1.fq
+open(FASTB, $ARGV[0]); #input R1.fq
 while(my$line=<FASTB>){
 	$tracker++;
+	#retrieve mates of reads that contain required barcodes above
 	if(defined $pas{$tracker}){print OUT2 "$line";}
 }close(OUT2);
