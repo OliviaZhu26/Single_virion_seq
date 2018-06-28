a89663e3ec38cbf8670fc80b7b8dc16216ad4271
OliviaZhu26 <30611503+OliviaZhu26@users.noreply.github.com>
2018-06-28 13:27:57 +0800

    Update Bam_to_mpileup.pl

--- BAsE-Seq_scripts/Bam_to_mpileup.pl
+++ BAsE-Seq_scripts/Bam_to_mpileup.pl
@@ -1,18 +1,22 @@
 use warnings;
 use strict;
 
+## Author: Lewis HONG and ZHU O. Yuan
+## Script name: Bam_to_mpileup.pl
+## OBTAIN PILEUP FILES FROM BAM FILES
+## Run as: perl $SCRIPTPATH/Bam_to_mpileup.pl $REFPATH $DATAPATH
+
 my$REF=$ARGV[0];
 my$DIRECTORY=$ARGV[1];
 
-### OBTAIN VCF FILES FROM BAM FILES
-
-system "mkdir $DIRECTORY/IndividualGenomes/pileup/";
-opendir(BAM, "$DIRECTORY/IndividualGenomes/sorted_bam_rmdup/");
+system "mkdir $DIRECTORY/IndividualGenomes/pileup/"; #creates new folder for pileup files
+opendir(BAM, "$DIRECTORY/IndividualGenomes/sorted_bam_rmdup/"); 
 my @files = readdir(BAM);
 foreach my $file (@files) {
 	if ($file =~ /(\w+)_rmdup\.bam/) {
+		#creating pileup file from bam
 		my $cmd = "samtools mpileup -f $REF $DIRECTORY/IndividualGenomes/sorted_bam_rmdup/$file > $DIRECTORY/IndividualGenomes/pileup/$1.pileup";
-		print "Processing $file\n";
+		print "Creating pileup for $file\n";
 		my @cmds = ("bash", "-c", $cmd);
 		system (@cmds);
 	}
