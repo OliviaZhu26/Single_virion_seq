4f76e742a16c989e6c9aec5c0ba62790728f2c32
OliviaZhu26 <30611503+OliviaZhu26@users.noreply.github.com>
2018-06-28 14:25:21 +0800

    Update 02_BAsE-Seq_haplotypes.sh

--- 02_BAsE-Seq_haplotypes.sh
+++ 02_BAsE-Seq_haplotypes.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-#determine cutoffs for BAsE-Seq library (refer to QC figures where necessary)
+##Author: ZHU O. Yuan
+##Script name: 02_BAsE-Seq_haplotypes.sh
+##BAsE-Seq pipeline script part 02 to generate consensus sequences for each barcode
+##Run as ./02_Base_seq_haplotype.sh 4 50 59 3198 3215 1 1500
 
-minCov=$1  #coverage per base
-percentCov=$2  #number of bases covered to minCov
-startPos=$3  #start position of area being considered for haplotype
-endpos=$4   #end position of area being considered for haplotype
-Rlen=$5    #length of reference 
+##Calls scripts: (please refer to individual scripts for details)
+# high_cov_genomes.pl
+# generate_consensus_seq.pl
+# haplo_list_all.pl
+
+minCov=$1  #coverage per base required for base call
+percentCov=$2  #percentage of bases covered to minCov for barcode sequence to be retained as a usable genome
+startPos=$3  #start position of area being considered for haplotype wrt reference genome
+endpos=$4   #end position of area being considered for haplotype wrt teference genome
+Rlen=$5    #length of reference genome
 cutoff=$6  #SNV frequency cutoff
 noGenomes=$7   #minimum number of haplotypes to constitute a well covered base across the population
 
 echo "Retrieving high coverage genomes"
 #Identify high coverage genomes (eg. at least 4 reads covering at least 50% of bases):
-perl $SCRIPTPATH/high_cov_genomes.pl $minCov $percentCov $startPos $endPos > $1_BarcodeRef.txt
+perl $SCRIPTPATH/high_cov_genomes.pl $minCov $percentCov $startPos $endPos > BarcodeRef_4x_50.txt
 #Generate individual genome sequences (eg. minimum depth = 4; minimum concordance = 0.7):
-perl $SCRIPTPATH/generate_consensus_seq.pl $minCov 0.7 individual_seq $RLen S7.1_BarcodeRef_4x_50.txt
-
-echo "Identify SNVs and construct haplotypes"
-#Obtain SNVs from individual genomes (eg. true SNV cutoff > 1%; "bad-call" cutoff = 1500 genomes):
-###perl $SCRIPTPATH/snv_from_genomes.pl  $REFPATH IndividualGenomes/individual_seq/ $startPos $endPos $cutoff $noGenomes Indiv_SNV_raw.txt Indiv_SNV_filtered.txt
-#Construct haplotypes from all genomes:
-perl $SCRIPTPATH/haplo_list_all.pl $REFPATH SNVs_all.txt IndividualGenomes/individual_seq/ Indiv_haplo.txt
+perl $SCRIPTPATH/generate_consensus_seq.pl $minCov 0.7 $RLen BarcodeRef_4x_50.txt > ConsensusSeqs.txt
